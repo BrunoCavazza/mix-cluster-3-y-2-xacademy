c9efcb189f862d4d5c0c2f7cb39859fd4f9a4896
gastonnievas <gastonnievas@gmail.com>
2023-08-13 21:42:41 -0300

    Form_Angular_Material
    Actualización con Angular Material: Checkbox, radio, input, card.

--- front/src/app/pages/form/form.component.ts
+++ front/src/app/pages/form/form.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {JsonPipe} from '@angular/common';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {JsonPipe, NgFor} from '@angular/common';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatRadioModule} from '@angular/material/radio';
+
+
 
 /** @title Checkboxes with reactive forms */
 @Component({
@@ -9,9 +17,31 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
   templateUrl: './form.component.html',
   styleUrls: ['form.component.css'],
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, MatCheckboxModule, JsonPipe],
+  imports: [FormsModule, ReactiveFormsModule, MatCheckboxModule, JsonPipe,
+    MatCardModule, MatButtonModule, MatInputModule,
+    MatFormFieldModule, MatStepperModule, MatRadioModule, NgFor],
 })
 export class FormComponent {
+  FormGroup1 = this._formBuilder.group({
+    Ctrl1: ['', Validators.required],
+  });
+  sexos: string[] = ['Masculino', 'Femenino'];
+  sexoElegido: string; // VERIFICAR COMO ESCRIBIR ESTO
+  turista = this._formBuilder.group({
+    edad: "",
+    masculino: false,
+    femenino: false,
+    cordoba: false,
+    otraCiudad: false,
+    otraProvincia: false,
+    otroPais: false,
+    ciudad: "",
+    provincia: "",
+    pais: ""
+  });
+  FormGroup2 = this._formBuilder.group({
+    Ctrl2: ['', Validators.required],
+  });
   difusion = this._formBuilder.group({
     television: false,
     pagina: false,
@@ -22,5 +52,56 @@ export class FormComponent {
     otros: "",
   });
 
-  constructor(private _formBuilder: FormBuilder) {}
+  FormGroup3 = this._formBuilder.group({
+    Ctrl3: ['', Validators.required],
+  });
+  FormGroup4 = this._formBuilder.group({
+    Ctrl4: ['', Validators.required],
+  });
+  FormGroup5 = this._formBuilder.group({
+    Ctrl5: ['', Validators.required],
+  });
+  FormGroup6 = this._formBuilder.group({
+    Ctrl6: ['', Validators.required],
+  });
+  FormGroup7 = this._formBuilder.group({
+    Ctrl7: ['', Validators.required],
+  });
+  FormGroup8 = this._formBuilder.group({
+    Ctrl8: ['', Validators.required],
+  });
+  FormGroup9 = this._formBuilder.group({
+    Ctrl9: ['', Validators.required],
+  });
+  FormGroup10 = this._formBuilder.group({
+    Ctrl10: ['', Validators.required],
+  });
+  FormGroup11 = this._formBuilder.group({
+    Ctrl11: ['', Validators.required],
+  });
+  FormGroup12 = this._formBuilder.group({
+    Ctrl12: ['', Validators.required],
+  });
+  FormGroup13 = this._formBuilder.group({
+    Ctrl13: ['', Validators.required],
+  });
+  FormGroup14 = this._formBuilder.group({
+    Ctrl14: ['', Validators.required],
+  });
+  FormGroup15 = this._formBuilder.group({
+    Ctrl15: ['', Validators.required],
+  });
+  FormGroup16 = this._formBuilder.group({
+    Ctrl16: ['', Validators.required],
+  });
+  FormGroup17 = this._formBuilder.group({
+    Ctrl17: ['', Validators.required],
+  });
+  FormGroup18 = this._formBuilder.group({
+    Ctrl18: ['', Validators.required],
+  });
+  constructor(private _formBuilder: FormBuilder) {
+    this.sexoElegido = "d";
+  }
 }
+
